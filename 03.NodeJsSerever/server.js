2c988338dabdc2eaa5f8734d97f6b697bf6c2484
Rustemhak <rustemhak@mail.ru>
2020-07-17 13:17:25 +0300

    Better adapt

--- 03.NodeJsSerever/server.js
+++ 03.NodeJsSerever/server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const app = express();
 // говорим, что мы раздаем папку public
 app.use(express.static('public'));
-
 // говорим, что запускаемся на порту 80
 app.listen(80);
 console.log("Server started at 80");
